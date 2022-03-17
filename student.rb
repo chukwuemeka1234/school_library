f0745f5993d90dd2b74f31974f46750d9ad3d491
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2022-03-17 11:14:09 +0100

    Add classes and methods

--- student.rb
+++ student.rb
@@ -1,14 +1,22 @@
 require_relative 'person'
 
 class Student < Person
-  attr_accessor :classroom
+  attr_reader :classroom
 
   def initialize(age, classroom, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @classroom = classroom
   end
 
+  def classroom=(studentsclass)
+    @classroom = studentsclass
+    studentsclass.students.push(self) unless studentsclass.students.include?(self)
+  end
+
   def play_hooky
     puts "¯\(ツ)/¯"
   end
 end
+
+student = Student.new(20, 'victor')
+puts student.name
--- teacher.rb
+++ teacher.rb
@@ -1,14 +1,22 @@
 require_relative 'person'
 
 class Teacher < Person
-  attr_accessor :specialization
+  attr_reader :classroom
 
   def initialize(age, specialization, name = 'Unknown', parent_permission: true)
     super(age, name, parent_permission)
     @specialization = specialization
   end
 
+  def classroom=(studentsclass)
+    @classroom = studentclass
+    studentsclass.students.push(self) unless studentsclass.students.include?(self)
+  end
+
   def can_use_services?
     true
   end
 end
+
+student = Teacher.new('A', 20)
+puts student.age
