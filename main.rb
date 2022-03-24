f1a94e9cf0fd2c7b945f3f80e65ccd6feaf96748
chukwuemeka1234 <anivictor.chukwuemeka.avc@gmail.com>
2022-03-24 08:26:29 +0100

    Refactor the code

--- main.rb
+++ main.rb
@@ -1,3 +1,48 @@
+require './app'
+
+def main
+  app = App.new
+
+  response = nil
+
+  puts "Welcome to school Library App!\n\n"
+  while response != '7'
+    puts 'Please choose an option by entering a number:'
+    puts '1 - List all books'
+    puts '2 - List all people'
+    puts '3 - Create a person'
+    puts '4 - Create a book'
+    puts '5 - Create a rental'
+    puts '6 - List all rentals for a given person'
+    puts '7 - Exit'
+    response = gets.chomp
+
+    case response
+    when '1'
+      app.list_books
+    when '2'
+      app.list_people
+    when '3'
+      app.create_person
+    when '4'
+      app.create_book
+    when '5'
+      app.create_rental
+    when '6'
+      app.list_rental
+    when '7'
+      puts 'Thank you for using this app!'
+    end
+
+    puts "\n"
+  end
+end
+
+main
+
+
+
+
 require_relative 'person'
 require_relative 'student'
 require_relative 'rental'
@@ -40,7 +85,7 @@ class App
       create_person
     when '4'
       create_book
-    when 5
+    when '5'
       create_rental
     when '6'
       list_rentals
